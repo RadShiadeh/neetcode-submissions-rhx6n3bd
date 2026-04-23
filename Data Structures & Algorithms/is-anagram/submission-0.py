918078a0577944dd9a6c545e07ae82e646b02e4c
Rad Shiadeh <78790490+RadShiadeh@users.noreply.github.com>
2026-04-23 14:16:06 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        t_chars = {}
+        s_chars = {}
+
+        for i in range(len(s)):
+            cs = s[i]
+            ct = t[i]
+            
+            t_chars[ct] = t_chars.get(ct, 0) + 1
+            s_chars[cs] = s_chars.get(cs, 0) + 1
+        
+        for c, f in s_chars.items():
+            if c not in t_chars or t_chars.get(c, 0) != s_chars[c]:
+                return False
+        
+        return True
