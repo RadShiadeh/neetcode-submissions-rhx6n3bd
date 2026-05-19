3db1db7e9a86d4af8eaa3c0adda0dc4f928402e1
Rad Shiadeh <78790490+RadShiadeh@users.noreply.github.com>
2026-05-19 14:18:14 +0100

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,59 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+    def insert(self, word):
+        node = self
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.end = True
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        if not words or len(words) == 0:
+            return []
+
+        trie = TrieNode()
+        for w in words:
+            trie.insert(w)
+
+        res = []
+        seen = set()
+        len_rows = len(board)
+        len_cols = len(board[0])
+
+        for i in range(len_rows):
+            for j in range(len_cols):
+               res, seen = self._backtrack(board, trie, "", i, j, res, seen)
+
+        return res
+
+    def _backtrack(self, board, node, word, i, j, res, seen):
+        if (
+            (i < 0 or i >= len(board))
+            or (j < 0 or j >= len(board[i]))
+            or board[i][j] not in node.children
+            or (i, j) in seen
+        ):
+            return res, seen
+        
+        c = board[i][j]
+        seen.add((i, j))
+        next_node = node.children[c]
+        word += c
+        if next_node.end:
+            res.append(word)
+            next_node.end = False
+        
+        res, seen = self._backtrack(board, next_node, word, i, j-1, res, seen)
+        res, seen = self._backtrack(board, next_node, word, i, j+1, res, seen)
+        res, seen = self._backtrack(board, next_node, word, i-1, j, res, seen)
+        res, seen = self._backtrack(board, next_node, word, i+1, j, res, seen)
+
+        seen.remove((i, j))
+
+        return res, seen
